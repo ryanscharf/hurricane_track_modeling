cf3abaf5e77f0b41a2b76a00715db75304875389
ryanscharf <ryan.scharf@gmail.com>
2022-06-17 19:28:21 -0400

    shoved in all the selectors

--- hurricane_modeling/app.R
+++ hurricane_modeling/app.R
@@ -28,50 +28,87 @@ pacman::p_load(necessary_packages, character.only = T)
 tmp_file <- tempdir(check = T)
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("hurrdydurdycane"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-          fluidRow(
-            column(3,
-                   textInput(inputId = "Year",
-                             label = "Year:",
-                             value = "2021",
-                             width = "100px"),
-                   textInput(inputId = "StormNum",
-                             label = "Storm Number:",
-                             value = "10",
-                             width = "100px"),
-                   textInput(inputId = "",
-                             label = "Year:",
-                             value = "2021",
-                             width = "100px")
-                   ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-          leafletOutput("mcleaflet")
-        )
+ui <- fluidPage(# Application title
+  titlePanel("hurrdydurdycane"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(sidebarPanel(
+    fluidRow(column(
+      3,
+      numericInput(
+        inputId = "Year",
+        label = "Year:",
+        value = 2021
+      ),
+      numericInput(
+        inputId = "StormNum",
+        label = "Storm Number:",
+        value = 10
+      ),     
+      numericInput(
+        inputId = 'Month',
+        label = 'Month: ',
+        value = 6,
+        min = 1,
+        max = 12
+      ),
+      numericInput(
+        inputId = 'Day',
+        label = 'Day: ',
+        value = 1,
+        min = 1,
+        max = 31
+      ),
+      numericInput(
+        inputId = 'Hour',
+        label = 'Hour: ',
+        value = '18',
+        min = 0,
+        max = 24
+      ),
+      numericInput(
+        inputId = 'Rmax',
+        label = 'Rmax: ',
+        value = 29
+      ),
+      numericInput(
+        inputId = 'IntensityMod',
+        label = 'Intensity Modifier: ',
+        value = 1.4
+      ),
+      numericInput(
+        inputId = 'ForecastHour',
+        label = 'Forecast Hour: ',
+        value = 120,
+        min = 0,
+        max = 172
+      ),
+      checkboxInput(
+        inputId = 'CurrentModels',
+        label = 'Use Only Current Models: ',
+        value = F
+      ),
+      checkboxInput(
+        inputId = 'DecayWindspeed',
+        label = 'Decay Windspeeds: ',
+        value = T
+      ),
+      numericInput(
+        inputId = 'NumClusters',
+        label = 'Number of Clusters: ',
+        value = 3
+      )
+      
     )
-)
-)
-)
+    )
+    ),
+    # Show a plot of the generated distribution
+    mainPanel(leafletOutput("mcleaflet"))
+  ))
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+  map <- leaflet() %>% addTiles() %>% setView(-93.65, 42.0285, zoom = 17)
+  output$mcleaflet <- renderLeaflet(map)
 }
 
 # Run the application 
